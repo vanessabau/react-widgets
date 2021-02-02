1f40359d7ea34f1c7580f161fb5960426ce47172
Vanessa Bautista <vbautista@rcglogistics.com>
2021-02-02 07:00:35 -0800

    added a search component with wikipedia api call

---
+++ widgets-app/src/components/Search.js
@@ -0,0 +1,73 @@
+import React, {
+  useState,
+  useEffect,
+} from "react";
+import axios from "axios";
+
+const Search = () => {
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+
+  console.log(results);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get(
+        "https://en.wikipedia.org/w/api.php",
+        {
+          params: {
+            action: "query",
+            list: "search",
+            origin: "*",
+            format: "json",
+            srsearch: term,
+          },
+        }
+      );
+      //Drill down to return the results we want
+      setResults(data.query.search);
+    };
+
+    //If term has characters inside it the search will run; otherwise you can provide default search content above in term
+    if (term) {
+      search();
+    }
+  }, [term]);
+
+  const renderedResults = results.map(
+    (result) => {
+      return (
+        <div className="item" key={result.pageid}>
+          <div className="content">
+            <div className="header">
+              {result.title}
+            </div>
+            {result.snippet}
+          </div>
+        </div>
+      );
+    }
+  );
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) =>
+              setTerm(e.target.value)
+            }
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
