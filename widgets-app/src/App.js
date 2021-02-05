3f9e26215dc690856a2e83419ce18095985680d5
Vanessa Bautista <vbautista@rcglogistics.com>
2021-02-05 06:00:16 -0800

    completed dropdown component

--- widgets-app/src/App.js
+++ widgets-app/src/App.js
@@ -1,28 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 // import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
-const items = [
+const options = [
   {
-    title: "What is React?",
-    content: "React is a front end framework",
+    label: "The Color Red",
+    value: "red",
   },
   {
-    title: "Why use React?",
-    content:
-      "React is a favorite JS library among engineers",
+    label: "The Color Green",
+    value: "green",
   },
   {
-    title: "How do you use React?",
-    content:
-      "You use React by creating components",
+    label: "A Shade of Blue",
+    value: "blue",
   },
 ];
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  const [selected, setSelected] = useState(
+    options[0]
+  );
+  const [
+    showDropdown,
+    setShowDropdown,
+  ] = useState(true);
+
   return (
     <div>
-      <Search />
+      <button
+        onClick={() =>
+          setShowDropdown(!showDropdown)
+        }
+      >
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
+
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a front end framework",
+//   },
+//   {
+//     title: "Why use React?",
+//     content:
+//       "React is a favorite JS library among engineers",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content:
+//       "You use React by creating components",
+//   },
+// ];
